032123d803e5ddc895444a5bbd86e6e266ab0ad8
Wintermourn <gladosnwheatley@gmail.com>
2026-07-13 01:32:32 -0500

    remove logging from registry validation

--- Data/Script/pmdorand/randomizer/registries/components.lua
+++ Data/Script/pmdorand/randomizer/registries/components.lua
@@ -4,7 +4,6 @@ local component = require 'pmdorand.randomizer.core.component'
 
 registries.create('components', 
     function(obj)
-        print(type(obj), getmetatable(obj) == component.meta.component, obj.id)
         return type(obj) == 'table' and getmetatable(obj) == component.meta.component and obj.id ~= '' and obj.provider_id ~= ''
     end,
     function(o)
--- Data/Script/pmdorand/randomizer/registries/providers.lua
+++ Data/Script/pmdorand/randomizer/registries/providers.lua
@@ -3,7 +3,6 @@ local provider = require 'pmdorand.randomizer.core.provider'
 
 registries.create('providers', 
     function(obj)
-        print(type(obj), getmetatable(obj) == provider.meta.provider, obj.id)
         return type(obj) == 'table' and getmetatable(obj) == provider.meta.provider and obj.id ~= ''
     end,
     function(o)
