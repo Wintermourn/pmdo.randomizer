b0bf7eb50f3dec7ecbcf293cb660fd0573254dd2
Wintermourn <gladosnwheatley@gmail.com>
2026-07-14 21:03:01 -0500

    wrap components in Config.Feature, split core config into public and personal

--- Data/Script/pmdorand/randomizer/cache/configurations.lua
+++ Data/Script/pmdorand/randomizer/cache/configurations.lua
@@ -1,12 +1,32 @@
+local config = require 'pmdorand.config'
 local interlace = require 'lib.pmdorand.interlace'
 local header = interlace .get_active_mod_by_uuid '019f4afe-e16e-734e-aebb-05e908454357'
 local nyaml = require 'lib.pmdorand.nyaml'
 nyaml(header.Path, 'Libraries', 'SharpYaml.dll')
 
 local IO = luanet.namespace 'System.IO'
+local illegalChars = IO.Path.GetInvalidFileNameChars()
+local illegalPattern = '['
+do
+    local char, code
+    for i = 0, illegalChars.Length - 1 do
+        char = string.char(illegalChars[i])
+
+        if string.find(char, '[%$%%%^%*%(%)%-%+%.%?]') then
+            illegalPattern = illegalPattern .. '%'.. char
+        else
+            illegalPattern = illegalPattern .. char
+        end
+    end
+    illegalPattern = illegalPattern .. ']'
+end
 
 local cache = {
-    core = {},
+    core = {
+        public = {},
+        personal = {}
+    },
+    ---@type {[string]: Config.Feature}
     components = {},
     structures = {
         core = require 'pmdorand.randomizer.core.settings' .structure,
@@ -37,14 +57,16 @@ local public = {}
 
 ---@param structure Config.FromTable
 function public.publish( component_id, structure )
-    cache.structures.components[component_id] = structure
-    cache.components[component_id] = make_default( structure )
+    local wrapped =config.feature( structure )
+    cache.structures.components[component_id] = wrapped
+    cache.components[component_id] = wrapped:get_default_value()
 end
 
 function public.construct_defaults()
-    cache.core = make_default( cache.structures.core )
+    cache.core.personal = make_default( cache.structures.core.personal )
+    cache.core.public = make_default( cache.structures.core.public )
     for i,k in pairs(cache.structures.components) do
-        cache.components[i] = make_default( k )
+        cache.components[i] = k:get_default_value()
     end
     return { core = cache.core, components = cache.components }
 end
@@ -63,8 +85,8 @@ local base_path = IO.Path.Combine(
 )
 
 ---@param name string
-function public.save( name )
-    local save_path = IO.Path.Combine(base_path, name ..'.yml')
+function public.save( name, metadata )
+    local save_path = IO.Path.Combine(base_path, name:gsub(illegalPattern, '_') ..'.yml')
     local path = IO.Path.GetFullPath( save_path )
     if path:sub(1, #base_path) ~= base_path then
        return false
@@ -72,19 +94,46 @@ function public.save( name )
     if not IO.Directory.Exists(base_path) then
         IO.Directory.CreateDirectory(base_path)
     end
+
+    if metadata == nil then
+        metadata = {
+            name = name
+        } 
+    end
+
     IO.File.WriteAllText(save_path, nyaml.serialize(
         setmetatable({
             versioning = {
                 mod = header.Version:ToString(),
                 game = interlace.get_game_header().Version:ToString()
             },
-            core = cache.core,
+            metadata = metadata or {
+                name = name
+            },
+            core = cache.core.public,
             components = cache.components
         }, {
             __nyamlKeyOrder = {
                 'versioning',
+                'metadata',
                 'core',
                 'components'
+            }
+        })
+    ))
+end
+
+function public.save_core_settings()
+    local path = IO.Path.Combine(base_path, '..', 'settings.yml')
+
+    IO.File.WriteAllText(path, nyaml.serialize(
+        setmetatable({
+            public = cache.core.public,
+            personal = cache.core.personal
+        }, {
+            __nyamlKeyOrder = {
+                'public',
+                'personal'
             }
         })
     ))
--- Data/Script/pmdorand/randomizer/core/settings/structure.lua
+++ Data/Script/pmdorand/randomizer/core/settings/structure.lua
@@ -1,8 +1,16 @@
 local config = require 'pmdorand.config'
 
 return {
-    always_flush = config.boolean(false),
-    export_to_mod = config.boolean(false),
-    log_spoilers = config.boolean(true),
-    enforce_config_limits = config.boolean(false)
+    personal = {
+        default_name = config.any(
+            config.null(),
+            config.string('')
+        ),
+        always_flush = config.boolean(false),
+        export_to_mod = config.boolean(false),
+        log_spoilers = config.boolean(true)
+    },
+    public = {
+        enforce_config_limits = config.boolean(false)
+    }
 }
